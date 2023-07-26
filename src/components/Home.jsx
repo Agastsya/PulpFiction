019144e3be634363e220af8a9af18aa012b74e48
agastsya <agastsya007@gmail.com>
2023-07-26 21:00:03 +0530

    Home jsx is completed and looks beautiful

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Box, Heading, Image } from '@chakra-ui/react';
+import { Stack, HStack, VStack, Text } from '@chakra-ui/react';
+import { Box, Container, Heading, Image } from '@chakra-ui/react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import img1 from '../assets/1.jpg';
@@ -20,6 +21,38 @@ const Home = () => {
   return (
     <Box>
       <MyCarousel />
+      <Container maxW={'container.xl'} minH={'100vh'} p={'16'}>
+        <Heading
+          w={'fit-content'}
+          py={'2'}
+          textTransform={'uppercase'}
+          borderBottom={'2px solid'}
+          margin={'auto'}
+        >
+          Services
+        </Heading>
+        <Stack
+          alignItems={'center'}
+          h={'full'}
+          direction={['column', 'row']}
+          p={'4'}
+        >
+          <Image
+            src={img5}
+            h={['40', '400']}
+            filter={'hue-rotate(90deg)'}
+          ></Image>
+          <Text letterSpacing={'widest'} lineHeight={'8'} px={'10'}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Text>
+        </Stack>
+      </Container>
     </Box>
   );
 };
@@ -57,13 +90,6 @@ const MyCarousel = () => {
 
       <Box w={'full'} h={'100vh'}>
         <Image src={img4} />
-        <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
-          Watch the Future
-        </Heading>
-      </Box>
-
-      <Box w={'full'} h={'100vh'}>
-        <Image src={img5} />
         <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
           Watch the Future
         </Heading>
