094c1321516d989869f806f093b294d4e087fe1c
agastsya <agastsya007@gmail.com>
2023-07-29 23:03:42 +0530

    added some edits to test

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,14 +1,28 @@
-import { Container } from '@chakra-ui/react';
-import React from 'react';
+import React, { Component } from 'react';
+import { Box, Container, Input, VStack, Stack, Center } from '@chakra-ui/react';
 
-const Test = () => {
-  return (
-    <Container w={'100%'} h={'100vh'}>
-      <form>
-        <input type="text" />
-      </form>
-    </Container>
-  );
-};
+class Test extends Component {
+  n = 'agastsya';
+
+  render() {
+    return (
+      <Box h={'100vh'}>
+        <Stack
+          direction={'column'}
+          h={'80vh'}
+          justifyContent={'center'}
+          align={'center'}
+        >
+          <VStack w={'60'}>
+            <Input type={'email'} w={'60'} placeholder="Email" />
+          </VStack>
+          <VStack w={'60'}>
+            <Input type={'password'} w={'60'} placeholder="Password" />
+          </VStack>
+        </Stack>
+      </Box>
+    );
+  }
+}
 
 export default Test;
